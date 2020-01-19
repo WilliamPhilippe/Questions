10518d5c2dcde719855d9b7615374a3367252104
William Philippe <wplb@ic.ufal.br>
2020-01-18 22:29:24 -0300

    HAHAHAH resolvi

--- PAA/TopologicalSort.cpp
+++ PAA/TopologicalSort.cpp
@@ -1,121 +1,110 @@
-// A C++ program to print topological sorting of a graph
-// using indegrees.
 #include <bits/stdc++.h>
+
 using namespace std;
 
-// Class to represent a graph
-class Graph
+int n, m;
+int x, y;
+
+void remove(int i, queue<int> &list)
 {
-  int V; // No. of vertices'
+  int init = list.front();
 
-  // Pointer to an array containing adjacency listsList
-  list<int> *adj;
-
-public:
-  Graph(int V); // Constructor
-
-  // function to add an edge to graph
-  void addEdge(int u, int v);
-
-  // prints a Topological Sort of the complete graph
-  void topologicalSort();
-};
-
-Graph::Graph(int V)
-{
-  this->V = V;
-  adj = new list<int>[V];
-}
-
-void Graph::addEdge(int u, int v)
-{
-  adj[u].push_back(v);
-}
-
-// The function to do Topological Sort.
-void Graph::topologicalSort()
-{
-  // Create a vector to store indegrees of all
-  // vertices. Initialize all indegrees as 0.
-  vector<int> in_degree(V, 0);
-
-  // Traverse adjacency lists to fill indegrees of
-  // vertices.  This step takes O(V+E) time
-  for (int u = 0; u < V; u++)
+  if (init == i)
   {
-    list<int>::iterator itr;
-    for (itr = adj[u].begin(); itr != adj[u].end(); itr++)
-      in_degree[*itr]++;
-  }
-
-  // Create an queue and enqueue all vertices with
-  // indegree 0
-  queue<int> q;
-  for (int i = 0; i < V; i++)
-    if (in_degree[i] == 0)
-      q.push(i);
-
-  // Initialize count of visited vertices
-  int cnt = 0;
-
-  // Create a vector to store result (A topological
-  // ordering of the vertices)
-  vector<int> top_order;
-
-  // One by one dequeue vertices from queue and enqueue
-  // adjacents if indegree of adjacent becomes 0
-  while (!q.empty())
-  {
-    // Extract front of queue (or perform dequeue)
-    // and add it to topological order
-    int u = q.front();
-    q.pop();
-    top_order.push_back(u);
-
-    // Iterate through all its neighbouring nodes
-    // of dequeued node u and decrease their in-degree
-    // by 1
-    list<int>::iterator itr;
-    for (itr = adj[u].begin(); itr != adj[u].end(); itr++)
-
-      // If in-degree becomes zero, add it to queue
-      if (--in_degree[*itr] == 0)
-        q.push(*itr);
-
-    cnt++;
-  }
-
-  // Check if there was a cycle
-  if (cnt != V)
-  {
-    cout << "There exists a cycle in the graph\n";
+    list.pop();
     return;
   }
 
-  // Print topological order
-  for (int i = 0; i < top_order.size(); i++)
-    cout << top_order[i] << " ";
-  cout << endl;
+  while (true)
+  {
+    int nu = list.front();
+    list.pop();
+    if (nu != i)
+    {
+      list.push(nu);
+    }
+
+    if (list.front() == init)
+      return;
+  }
 }
 
-// Driver program to test above functions
+void getZero(queue<int> *graph, queue<int> *invert, queue<int> &result, int *removed)
+{
+  priority_queue<int, vector<int>, greater<int>> pq;
+
+  while (true)
+  {
+
+    for (int i = 1; i <= n; i++)
+    {
+      if (graph[i].empty() && !removed[i])
+      {
+
+        removed[i] = 1;
+        pq.push(i);
+
+        while (!invert[i].empty())
+        {
+          int node = invert[i].front();
+          invert[i].pop();
+
+          remove(i, graph[node]);
+        }
+
+        break;
+      }
+    }
+
+    if (pq.empty())
+      return;
+
+    while (!pq.empty())
+    {
+      result.push(pq.top());
+      // while (!invert[pq.top()].empty())
+      // {
+      //   int node = invert[pq.top()].front();
+      //   invert[pq.top()].pop();
+
+      //   remove(pq.top(), graph[node]);
+      // }
+      pq.pop();
+    }
+  }
+}
+
 int main()
 {
 
-  int n, m;
   cin >> n >> m;
 
-  Graph g(n + 1);
+  // priority_queue<int, vector<int>, greater<int>> graph[n + 1];
+  queue<int> graph[n + 1];
+  queue<int> invert[n + 1];
+  queue<int> result;
+  int removed[n + 1] = {0};
 
-  while (m--)
+  for (int i = 0; i < m; i++)
   {
-    int x, y;
     cin >> x >> y;
-    g.addEdge(x, y);
+    graph[x].push(y);
+    invert[y].push(x);
   }
 
-  cout << "Following is a Topological Sort of\n";
-  g.topologicalSort();
+  getZero(graph, invert, result, removed);
+
+  if (result.size() != n)
+  {
+    cout << "Impossible";
+    return 0;
+  }
+
+  while (!result.empty())
+  {
+    cout << result.front() << " ";
+    result.pop();
+  }
 
   return 0;
 }
