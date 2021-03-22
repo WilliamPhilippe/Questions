4b518d9699f609b4d54798cf7cca292408bba5cb
William Philippe <wplb@ic.ufal.br>
2021-03-22 19:11:46 -0300

    add: plot-histogram-data-science-ativ-1-4-2020.1

--- Data-Science/data.py
+++ Data-Science/data.py
@@ -1,5 +1,11 @@
-v1 = [1, 2, 3, 4, 5, 6, 7, 8, 29]
-v2 = [3, 2, 1, -5, 5, 0, 0, 1, 3]
+# ------------------------------------------------------------------------------------
+# QUESTÃO 4 a) e b)
+# - - - 
+
+# LUCRO por ano (1 - 18) em milhões de reais
+
+v1 = [1, 2, 3, 4, 5, 6, 7, 8, 29, 1, 32, 3, 24, 15, 16, 71, 82, 29, 3]
+v2 = [3.1, 2.1, 1.1, -5.1, 2.10, 0, 0, 1.1, 3.1, 3.1, 2.1, 1.1, -2.10, 2.10, 0, 0, 10.1, 3.1, 0.3]
 
 media_v1 = 0
 media_v2 = 0
@@ -8,9 +14,30 @@ for x in range(0, len(v1)):
   media_v1 += v1[x]
   media_v2 += v2[x]
 
+# Resposta a):
 print("Média de V1", len(v1), ": ", media_v1/len(v1), " Mediana: ", v1[len(v1) // 2])
-print("Média de V2", len(v2), ": ", media_v2/len(v2), " Mediana: ", v2[len(v2) // 2])
+# Média de V1 19 :  17.94736842105263  Mediana:  1
 
-# RESULTADO
-# Média de V1 9 :  7.222222222222222  Mediana:  5
-# Média de V2 9 :  1.1111111111111112  Mediana:  5
+# Resposta b):
+print("Média de V2", len(v2), ": ", media_v2/len(v2), " Mediana: ", v2[len(v2) // 2])
+# Média de V2 19 :  1.436842105263158  Mediana:  3.1
+
+
+
+# ------------------------------------------------------------------------------------
+# QUESTÃO 5 a) e b)
+# - - - 
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.style.use('fivethirtyeight')
+
+plt.hist(v2, bins=6, edgecolor='black')
+
+plt.title('Vendas por ano (V2)')
+plt.xlabel('Milhões de reais')
+plt.ylabel('Quantidades')
+
+plt.tight_layout()
+
+plt.show()
